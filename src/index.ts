454a467322ba7613b993f28f2cd6df849f02c6a8
Rafael Rocha <rafadeveloper.contato@gmail.com>
2023-04-25 09:54:34 -0400

    feat: add winston debug

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import { createLogger, format, transports } from 'winston'
+
 console.log('Hello World')
+
+const logger = createLogger({
+  level: 'info',
+  format: format.json(),
+  transports: [
+    new transports.File({
+      filename: 'logs/app.log',
+      level: 'debug',
+    }),
+  ],
+})
+
+process.on('uncaughtException', (error) => {
+  logger.error(`Erro global capturado: ${error.stack}`)
+  process.exit(1)
+})
